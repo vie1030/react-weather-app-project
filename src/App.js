7894f0236b61c59c02de29d2cc675cab3533de87
vie1030 <91474368+vie1030@users.noreply.github.com>
2022-01-27 21:57:02 +0100

    added the GitHub repo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather React App</div>;
+  return (
+    <div className="App">
+      Weather React App
+      <footer>
+        This is{" "}
+        <a
+          href="https://github.com/vie1030/react-weather-app-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub open-sourced
+        </a>{" "}
+        project coded by{" "}
+        <a href="/" target="_blank" rel="noreferrer">
+          Vie Santos
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
