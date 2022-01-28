845ec2c18b25421f0b0861eb3192307736f03698
vie1030 <91474368+vie1030@users.noreply.github.com>
2022-01-28 17:24:23 +0100

    added bootstrap in my project

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather React App
-      <footer>
-        This is{" "}
-        <a
-          href="https://github.com/vie1030/react-weather-app-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub open-sourced
-        </a>{" "}
-        project coded by{" "}
-        <a href="/" target="_blank" rel="noreferrer">
-          Vie Santos
-        </a>
-      </footer>
+      <div className="container">
+        <h1 className="mb-3 mt-5">Weather React App</h1>
+        <button className="btn btn-primary">Hello</button>
+        <footer>
+          This is{" "}
+          <a
+            href="https://github.com/vie1030/react-weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub open-sourced
+          </a>{" "}
+          project coded by{" "}
+          <a href="/" target="_blank" rel="noreferrer">
+            Vie Santos
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
