370c6108e6d5607c555609c91d41a3ce3df62936
vie1030 <91474368+vie1030@users.noreply.github.com>
2022-02-09 20:38:47 +0100

    deleted the fahrenheit convertion

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -20,7 +20,7 @@ export default function DailyForecast(props) {
   if (loaded) {
     return (
       <div className="WeatherForecast">
-        <div className="row">
+        <div className="row mt-3">
           {forecast.map(function (dailyForecast, index) {
             if (index < 5) {
               return (
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,51 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function displayCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function displayFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function fahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
-
-  if (unit === "celsius") {
-    return (
-      <span className="WeatherTemperature">
-        {" "}
-        <span className="temperature">
-          {Math.round(props.celsius)}
-          <span className="unit">
-            °C |{" "}
-            <a href="/" onClick={displayFahrenheit}>
-              °F{" "}
-            </a>
-          </span>
-        </span>
-      </span>
-    );
-  } else {
-    return (
-      <span className="WeatherTemperature">
-        {" "}
-        <span className="temperature">
-          {Math.round(fahrenheit())}
-          <span className="unit">
-            <a href="/" onClick={displayCelsius}>
-              °C{" "}
-            </a>
-            | °F{" "}
-          </span>
-        </span>
-      </span>
-    );
-  }
+  return (
+    <span className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">°C</span>
+    </span>
+  );
 }
