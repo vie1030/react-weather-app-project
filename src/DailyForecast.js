57f57c37777454ea6343f14fc5f04a65ba7dc3ea
vie1030 <91474368+vie1030@users.noreply.github.com>
2022-02-09 15:22:04 +0100

    added the useEffect and the mapping

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
@@ -8,6 +8,10 @@ export default function DailyForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
@@ -15,23 +19,19 @@ export default function DailyForecast(props) {
 
   if (loaded) {
     return (
-      <div className="DailyForecast mt-3">
+      <div className="WeatherForecast">
         <div className="row">
-          <div className="col-sm">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
-          <div className="col-sm">
-            <WeatherForecastDay data={forecast[1]} />
-          </div>
-          <div className="col-sm">
-            <WeatherForecastDay data={forecast[2]} />
-          </div>
-          <div className="col-sm">
-            <WeatherForecastDay data={forecast[3]} />
-          </div>
-          <div className="col-sm">
-            <WeatherForecastDay data={forecast[4]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col-sm" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
       </div>
     );
